408d7c84584c13ee64371dc02148bd0d5548110b
NedheSingh <nidhimjsh@gmail.com>
2023-07-10 20:02:24 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="CSS/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+ </head>
+<body>
+
+<?php include 'menu.php'; ?>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/pngwing.png" alt="Delicious meal" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Delicious meal</h3>
+        <p>Pizza,French Fries,Nuggets</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/pngwing1.png" alt="Soup" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Soup</h3>
+        <p>Vegetable Soup</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/pngwing5.png" alt="Burger" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Burger</h3>
+        <p>Double patty-Harmburger</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Best Sellers</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12 ">
+        <div class="card" >
+          <img class="card-img-top" src="images/pngwing7.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Kebab</h4>
+            <p class="card-text">Rs.150</p>
+            <a href="#" class="btn btn-primary">Add to cart</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12 ">
+        <div class="card" >
+          <img class="card-img-top" src="images/pngwing7.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Kebab</h4>
+            <p class="card-text">Rs.250</p>
+            <a href="#" class="btn btn-primary">Add to cart</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12 ">
+        <div class="card" >
+          <img class="card-img-top" src="images/pngwing9.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Marghareta</h4>
+            <p class="card-text">Rs.260</p>
+            <a href="#" class="btn btn-primary">Add to cart</a>
+          </div>
+        </div>
+      </div>
+      
+
+    </div>
+  </div>
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Categories</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/pngwing1.png" class="img-fluid pb-4">
+        <h4 class="card-title" align="Center">Sweets</h4>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/pngwing1.png" class="img-fluid pb-4">
+        <h4 class="card-title" align="Center">Namkeen</h4>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/pngwing1.png" class="img-fluid pb-4">
+        <h4 class="card-title" align="Center">Beverages</h4>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/pngwing1.png" class="img-fluid pb-4">
+        <h4 class="card-title" align="Center">Cookies</h4>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/pngwing1.png" class="img-fluid pb-4">
+        <h4 class="card-title" align="Center">Ready To Eat</h4>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/pngwing1.png" class="img-fluid pb-4">
+        <h4 class="card-title" align="Center">Gift Boxes</h4>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">New Arrivals</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12 ">
+        <div class="card" >
+          <img class="card-img-top" src="images/pngwing7.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Kebab</h4>
+            <p class="card-text">Rs.150</p>
+            <a href="#" class="btn btn-primary">Add to cart</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12 ">
+        <div class="card" >
+          <img class="card-img-top" src="images/pngwing7.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Kebab</h4>
+            <p class="card-text">Rs.250</p>
+            <a href="#" class="btn btn-primary">Add to cart</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12 ">
+        <div class="card" >
+          <img class="card-img-top" src="images/pngwing9.png" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Marghareta</h4>
+            <p class="card-text">Rs.260</p>
+            <a href="#" class="btn btn-primary">Add to cart</a>
+          </div>
+        </div>
+      </div>
+      
+
+    </div>
+  </div>
+ </section>
+  
+<section class="my-5">
+  
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-12">
+        <img src="images/pngwing6.png" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12">
+      <h2 class="display-4">About Us</h2>
+      <p class="py-3 aboutpara">Over the course of eight decades, a lot has changed about us. We have relocated, undergone expansion, developed new product lines & added segments, opened retail chains & stores across India and embraced new markets overseas.
+      One thing hasn’t changed - we’re still a tight-knit family business, committed to serving the most authentic taste of India through our products.
+      Our origins can be traced back to a small namkeen shop in Bikaner founded by Ganga Bishan Agarwal (Haldiram Ji). This modest shop quickly gained popularity and scaled up to meet a booming demand for its unique-tasting bhujia. Building on this legacy, his grandson, our pioneer Mr. Shiv Kishan Agrawal steered the business towards the heights it has tasted today.</p>
+      <a href="about.php" class="btn btn-success"> Explore More</a>
+      </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Want To Get Latest Updates!</h2>
+    <h5 align="center">Sign Up For Free.</h5>
+  </div>
+  <div class="w-50 m-auto">
+    <form action="userinfo.php" method="post">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="user" autocomplete="off" class="form-control">
+      </div>
+       <div class="form-group">
+        <label>Email-Id</label>
+        <input type="text" name="email" autocomplete="off" class="form-control">
+      </div>
+       <div class="form-group">
+        <label>Mobile No.</label>
+        <input type="text" name="mobile" autocomplete="off" class="form-control">
+      </div>
+       <div class="form-group">
+        <label>Comments</label>
+        <textarea class="form-control" name="comments"></textarea>
+      </div>
+      <div>
+        <button type="submit" class="btn btn-success">Submit</button>
+      </div>
+    </form>
+  </div>
+</section>
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@fooddiesProduction</p>
+</footer>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">FOODDIES</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Sweets <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Savories</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="aboutphp">Beverages</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cookies</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Gifting</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search food-items" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
